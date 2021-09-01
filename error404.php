93dbf59e9b09461a7bd445da377d7f6b297cd878
ElectroMaster05 <al060878@uacam.mx>
2021-09-01 13:20:02 -0500

    version1

---
+++ error404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Error 404</title>
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="estilos.css" rel="stylesheet" type="text/css"><!-- linkeo css   --> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href='index.html'>
+
+
+</head>
+<body>
+
+ <div class="container-404">
+ 	
+<h1>Error 404</h1>
+
+<img src="images/error404.jpg" class="error404">   
+
+<p class="disculpa">Esta pagina no se encuentra disponible en estos momentos.</p>
+
+<a href="index.html" class="paginaprincipal">Pagina Principal </a>
+
+ </div>
+
+
+
+</body>
+</html>
